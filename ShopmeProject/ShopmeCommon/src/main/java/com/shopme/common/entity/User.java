20d7bca5dfd39349fb70d4816bcd38e64454fe6f
Djan Daudov <djandaudovv@gmail.com>
2022-08-13 19:18:30 +0300

    feat: code User Photos Upload Function

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
@@ -1,5 +1,6 @@
 package com.shopme.common.entity;
 
+import java.beans.Transient;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -132,4 +133,11 @@ public class User {
 				+ ", roles=" + roles + "]";
 	}
 	
+	@Transient
+	public String getPhotosImagePath() {
+		
+		if(id == null || photos == null) return "/images/default-user.png";
+		
+		return "/user-photos/" + this.id + "/" + this.photos;
+	}
 }
